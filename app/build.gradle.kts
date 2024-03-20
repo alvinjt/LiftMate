0ef678ee37c752e8a89fa8524ac33d1d090d34ea
andrew.heiar <aheiar@iastate.edu>
2024-03-20 15:06:13 -0500

    got workout button working. Make the workout activity look nice!!!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.1")
-    implementation("com.android.support:appcompat-v7:23.2.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/LiftMate/MainActivity.kt
+++ app/src/main/java/com/example/LiftMate/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.example.LiftMate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clickListener();
+    }
+
+    public fun clickListener(){
+        var imageWorkout = findViewById<ImageView>(R.id.buttonWorkouts);
+
+        imageWorkout.setOnClickListener{
+            openWorkoutPageActivity()
+        }
 
     }
+
+    public fun openWorkoutPageActivity(){
+        startActivity(Intent(this@MainActivity, WorkoutPageActivity::class.java))
+    }
+
 }

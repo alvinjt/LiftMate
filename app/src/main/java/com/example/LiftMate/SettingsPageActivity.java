970bde938951faf6d34b9addf35a25fa49e25e3b
Alejandro <aperezro@iastate.edu>
2024-04-10 11:28:36 -0500

    Merge remote-tracking branch 'origin/AndrewBranch' into Alejandrobranch
    Back button in settings page

--- app/src/main/java/com/example/LiftMate/SettingsPageActivity.java
+++ app/src/main/java/com/example/LiftMate/SettingsPageActivity.java
@@ -3,12 +3,26 @@ package com.example.LiftMate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsPageActivity extends AppCompatActivity {
 
+    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
+
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });{
+            onBackPressed();
+        }
     }
 }

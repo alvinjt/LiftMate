e01766618f42616ccf201444be01c9cb3825babb
caels <caelshep@iastate.edu>
2024-04-21 20:21:39 -0500

    Simple workout page with working buttons

--- app/src/main/java/com/example/LiftMate/WorkoutPageActivity.java
+++ app/src/main/java/com/example/LiftMate/WorkoutPageActivity.java
@@ -2,7 +2,10 @@ package com.example.LiftMate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WorkoutPageActivity extends AppCompatActivity {
 
@@ -11,4 +14,29 @@ public class WorkoutPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_page);
     }
+
+    public void navigateToChestPage (View view){
+        Intent intent = new Intent (this, ChestWorkoutActivity.class);
+        startActivity(intent);
+    }
+
+    public void navigateToLegsPage (View view){
+        Intent intent = new Intent (this, LegsWorkoutActivity.class);
+        startActivity(intent);
+    }
+
+    public void navigateToBackPage (View view){
+        Intent intent = new Intent (this, BackWorkoutActivity.class);
+        startActivity(intent);
+    }
+    public void navigateToArmsPage (View view){
+        Intent intent = new Intent (this, ArmsWorkoutActivity.class);
+        startActivity(intent);
+    }
+
+    public void navigateToCorePage (View view){
+        Intent intent = new Intent (this, CoreWorkoutActivity.class);
+        startActivity(intent);
+    }
+
 }

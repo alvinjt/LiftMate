91838d7dedf44fe1dbf8c9be4689ea72c87bada0
andrew.heiar <aheiar@iastate.edu>
2024-04-24 15:18:04 -0500

    Best partial version

--- app/src/main/java/com/example/LiftMate/AccountPageActivity.kt
+++ app/src/main/java/com/example/LiftMate/AccountPageActivity.kt
@@ -1,20 +1,26 @@
 package com.example.LiftMate
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.LiftMate.databinding.ActivityAccountPageBinding
-import com.example.LiftMate.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
+
 
 class AccountPageActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_account_page)
 
+        val userEmailInfoPlacehold = findViewById<TextView>(R.id.emailView)
 
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            val userEmail = user.email
+            userEmailInfoPlacehold.text = userEmail
+        } else {
+            // No user is signed in
+        }
     }
 }
--- app/src/main/java/com/example/LiftMate/SettingsPageActivity.java
+++ app/src/main/java/com/example/LiftMate/SettingsPageActivity.java
@@ -17,4 +17,6 @@ public class SettingsPageActivity extends AppCompatActivity {
 
 
     }
+
+
 }

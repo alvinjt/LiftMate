a8b532dd4904eb1e1d61101ec0851a97a943e8c4
andrew.heiar <aheiar@iastate.edu>
2024-04-24 14:10:51 -0500

    Workingish

--- app/src/main/java/com/example/LiftMate/AccountPageActivity.kt
+++ app/src/main/java/com/example/LiftMate/AccountPageActivity.kt
@@ -4,34 +4,32 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.LiftMate.databinding.ActivityAccountPageBinding
+import com.example.LiftMate.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
 
-
 class AccountPageActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityAccountPageBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityAccountPageBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_account_page)
-
+        setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
 
-        if(firebaseAuth.currentUser != null){
+        if (firebaseAuth.currentUser != null){
             firebaseAuth.currentUser?.let {
                 binding.emailView.text = it.email
             }
         }
-        binding.signOutBtn.setOnClickListener {
+        binding.BtnSignOut.setOnClickListener {
             firebaseAuth.signOut()
-
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
         }
-
+        finish()
     }
 }
